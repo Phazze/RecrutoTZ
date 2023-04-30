10bcd74880e617830404765e6ea46f0bf641bb58
Phazze <nekrashevich.gleb@gmail.com>
2023-05-01 00:31:59 +0300

    finish

--- recrutotz/main/views.py
+++ recrutotz/main/views.py
@@ -5,7 +5,13 @@ from django.http import HttpResponse
 def main(requests):
     name = requests.GET.get("name")
     message = requests.GET.get("message")
+    contex = {
+        "name": name,
+        "message": message
+    }
     if name == None or message == None:
-        name = 'Recruto'
-        message = 'Давай дружить'
-    return HttpResponse(f'<h1>Hello {name}! </h1><br> <h1>{message}<h1>')
+        contex = {
+            "name": 'Recruto',
+            "message": "Давай дружить"
+        }
+    return render(requests, 'main/main.html', contex)
